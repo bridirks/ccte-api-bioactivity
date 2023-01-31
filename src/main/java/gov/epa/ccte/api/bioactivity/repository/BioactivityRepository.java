7f46a016092f4f5ad8104053fcf53211be860166
Asif Rashid <rashid.asif@epa.gov>
2023-01-31 17:32:29 -0500

    Added apikey, test code, dto

--- src/main/java/gov/epa/ccte/api/bioactivity/repository/BioactivityRepository.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/repository/BioactivityRepository.java
@@ -3,14 +3,17 @@ package gov.epa.ccte.api.bioactivity.repository;
 import gov.epa.ccte.api.bioactivity.domain.Bioactivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RepositoryRestResource(exported = false)
 public interface BioactivityRepository extends JpaRepository<Bioactivity, Integer> {
 
+    @Transactional(readOnly = true)
     List<Bioactivity> findByDtxsid(String dtxsid);
 
+    @Transactional(readOnly = true)
     List<Bioactivity> findByAeid(Integer aeid);
 
 }
--- src/main/java/gov/epa/ccte/api/bioactivity/web/rest/BioactivityResource.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/web/rest/BioactivityResource.java
@@ -1,25 +1,37 @@
 package gov.epa.ccte.api.bioactivity.web.rest;
 
 import gov.epa.ccte.api.bioactivity.domain.Bioactivity;
+import gov.epa.ccte.api.bioactivity.dto.BioactivityDto;
+import gov.epa.ccte.api.bioactivity.dto.mapper.BioactivityMapper;
 import gov.epa.ccte.api.bioactivity.repository.BioactivityRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * REST controller for getting the {@link gov.epa.ccte.api.bioactivity.domain.Bioactivity}s.
  */
+@Tag(name = "Bioactivity Resource",
+        description = "API endpoints for collecting Bioactivity data for specified chemical identifier (DTXSID).")
+@SecurityRequirement(name = "api_key")
 @Slf4j
 @RestController
+@CrossOrigin
 public class BioactivityResource {
 
     final private BioactivityRepository repository;
+    final private BioactivityMapper mapper;
 
-    public BioactivityResource(BioactivityRepository repository) {
+    public BioactivityResource(BioactivityRepository repository, BioactivityMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     /**
@@ -28,13 +40,17 @@ public class BioactivityResource {
      * @param dtxsid the matching dtxsid of the Bioactivity to retrieve.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the list of chemicalDetail}.
      */
+    @Operation(summary = "Get bioactivity data by dtxsid")
     @RequestMapping(value = "bioactivity/search/by-dtxsid/{dtxsid}", method = RequestMethod.GET)
     public @ResponseBody
-    List<Bioactivity> bioactivityByDtxsid(@PathVariable("dtxsid") String dtxsid) throws IOException {
+    List<BioactivityDto> bioactivityByDtxsid(@PathVariable("dtxsid") String dtxsid) throws IOException {
 
         log.debug("dtxsid = {}", dtxsid);
+        List<Bioactivity> data = repository.findByDtxsid(dtxsid);
 
-        return repository.findByDtxsid(dtxsid);
+        return data.stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -43,13 +59,18 @@ public class BioactivityResource {
      * @param aeid the matching aeid of the Bioactivity to retrieve.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the list of bioactivity}.
      */
+    @Operation(summary = "Get bioactivity data by aeid")
     @RequestMapping(value = "bioactivity/search/by-aeid/{aeid}", method = RequestMethod.GET)
     public @ResponseBody
-    List<Bioactivity> bioactivityByAeid(@PathVariable("aeid") Integer aeid) throws IOException {
+    List<BioactivityDto> bioactivityByAeid(@PathVariable("aeid") Integer aeid) throws IOException {
 
         log.debug("aeid = {}", aeid);
 
-        return repository.findByAeid(aeid);
+        List<Bioactivity> data = repository.findByAeid(aeid);
+
+        return data.stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
 }
