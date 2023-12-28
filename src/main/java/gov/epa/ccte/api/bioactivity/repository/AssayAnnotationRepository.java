7b1185e73bdbb0ba5913159242ee2df5e41edba7
asif rashid <58010651+asif-rashid@users.noreply.github.com>
2023-12-28 12:47:19 -0500

    Implemented all assays endpoint #11 (#16)

--- src/main/java/gov/epa/ccte/api/bioactivity/repository/AssayAnnotationRepository.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/repository/AssayAnnotationRepository.java
@@ -4,9 +4,12 @@ import gov.epa.ccte.api.bioactivity.domain.AssayAnnotation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 @RepositoryRestResource(exported = false)
 public interface AssayAnnotationRepository extends JpaRepository<AssayAnnotation, Integer> {
     <T> T findByAeid(Integer aeid, Class<T> type);
 
+    <T> List<T> findBy(Class<T> type);
 }
