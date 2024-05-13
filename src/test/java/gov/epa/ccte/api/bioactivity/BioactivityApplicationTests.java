11f6629e00d9021646343f905cd2994cb2df0d27
asif rashid <58010651+asif-rashid@users.noreply.github.com>
2024-05-13 16:18:46 -0400

    26 implement data layer unit tests (#28)
    * Implemented data layer testing. #26
    
    * Implemented data layer testing. #26
    
    * Implemented data layer testing. #26

--- src/test/java/gov/epa/ccte/api/bioactivity/BioactivityApplicationTests.java
+++ src/test/java/gov/epa/ccte/api/bioactivity/BioactivityApplicationTests.java
@@ -1,13 +1,25 @@
 package gov.epa.ccte.api.bioactivity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-//@ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.yml")
+@Testcontainers
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class BioactivityApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> pgsqldb = new PostgreSQLContainer<>("postgres:13-alpine");
 
 	@Test
 	void contextLoads() {
