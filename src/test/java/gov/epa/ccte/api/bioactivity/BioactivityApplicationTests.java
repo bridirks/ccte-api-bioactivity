67106370adfffccc2cea216dbc1e1d31a1e60adf
asif rashid <rashid.asif@epa.gov>
2024-05-13 15:07:05 -0400

    Implemented data layer testing. #26

--- src/test/java/gov/epa/ccte/api/bioactivity/BioactivityApplicationTests.java
+++ src/test/java/gov/epa/ccte/api/bioactivity/BioactivityApplicationTests.java
@@ -3,9 +3,11 @@ package gov.epa.ccte.api.bioactivity;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("dev")
+//@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yml")
 class BioactivityApplicationTests {
 
 	@Test
