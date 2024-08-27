5f85b5631abc210a04e2a545fccefa24b694b760
bedirks <Dirks.Brianna@epa.gov>
2024-08-27 14:08:04 -0400

    extract interface AssayResource and add api documentation tags for resource #37

--- src/main/java/gov/epa/ccte/api/bioactivity/web/rest/AssayResource.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/web/rest/AssayResource.java
@@ -1,15 +1,9 @@
 package gov.epa.ccte.api.bioactivity.web.rest;
 
-import gov.epa.ccte.api.bioactivity.domain.AssayAnnotation;
 import gov.epa.ccte.api.bioactivity.projection.assay.AssayAll;
 import gov.epa.ccte.api.bioactivity.projection.assay.AssayBase;
 import gov.epa.ccte.api.bioactivity.repository.AssayAnnotationRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
