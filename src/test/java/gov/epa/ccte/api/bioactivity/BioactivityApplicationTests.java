7219ef01d524ffeb4285fdd40c1d80b9934c5a2e
bedirks <Dirks.Brianna@epa.gov>
2024-10-08 15:18:44 -0400

    Implemented test for BioactivityApplication #29

--- src/test/java/gov/epa/ccte/api/bioactivity/BioactivityApplicationTests.java
+++ src/test/java/gov/epa/ccte/api/bioactivity/BioactivityApplicationTests.java
@@ -2,11 +2,9 @@ package gov.epa.ccte.api.bioactivity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
