7384f088dc61d9905d0faeab8a5bf85ed784ea1d
Brianna Dirks <Dirks.Brianna@epa.gov>
2025-01-28 12:01:36 -0500

    Create POST method for all assay annotation endpoints (aeid) #13

--- src/main/java/gov/epa/ccte/api/bioactivity/web/rest/AssayResource.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/web/rest/AssayResource.java
@@ -2,8 +2,12 @@ package gov.epa.ccte.api.bioactivity.web.rest;
 
 import gov.epa.ccte.api.bioactivity.projection.assay.AssayAll;
 import gov.epa.ccte.api.bioactivity.projection.assay.AssayBase;
+import gov.epa.ccte.api.bioactivity.projection.data.BioactivityDataAll;
 import gov.epa.ccte.api.bioactivity.repository.AssayAnnotationRepository;
+import gov.epa.ccte.api.bioactivity.web.rest.error.HigherNumberOfRequestsException;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,7 +16,10 @@ import java.util.List;
 @RestController
 public class AssayResource implements AssayApi {
     final private AssayAnnotationRepository annotationRepository;
-
+    
+    @Value("200")
+    private Integer batchSize;
+    
     public AssayResource(AssayAnnotationRepository annotationRepository) {
         this.annotationRepository = annotationRepository;
     }
@@ -22,6 +29,19 @@ public class AssayResource implements AssayApi {
         log.debug("aeid = {}", aeid);
 
         AssayAll data = annotationRepository.findByAeid(aeid, AssayAll.class);
+
+        return data;
+    }
+    
+    @Override
+    public @ResponseBody
+    List<AssayAll>batchSearchAssayByAeid(String[] aeids) {
+        log.debug("bioactiivty data for aeid size = {}", aeids.length);
+
+        if(aeids.length > batchSize)
+            throw new HigherNumberOfRequestsException(aeids.length, batchSize);
+
+        List<AssayAll> data = annotationRepository.findByAeidInOrderByAeidAsc(aeids, AssayAll.class);
 
         return data;
     }
