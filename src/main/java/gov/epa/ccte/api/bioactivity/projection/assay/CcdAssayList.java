986cb8b9422b11b9cd505841401446c5afc8df40
SrikanthDeevi <deevi.srikanth@epa.gov>
2025-03-26 10:44:44 -0400

    CE-4537 Added ccd assay detail field for assay list details

--- src/main/java/gov/epa/ccte/api/bioactivity/projection/assay/CcdAssayList.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/projection/assay/CcdAssayList.java
@@ -16,6 +16,8 @@ public interface CcdAssayList extends AssayBase {
     
     String getAssayComponentEndpointDesc();
     
+    String getCcdAssayDetail();
+    
     String getEntrezGeneId();
     
     String getGeneName();
