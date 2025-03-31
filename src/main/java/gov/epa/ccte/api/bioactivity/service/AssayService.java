89780e20f649420b49092e1affe04e2a11dbef06
SrikanthDeevi <deevi.srikanth@epa.gov>
2025-03-31 14:05:50 -0400

    CE-4561 Made changes to pull all records of gene array

--- src/main/java/gov/epa/ccte/api/bioactivity/service/AssayService.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/service/AssayService.java
@@ -15,45 +15,45 @@ import lombok.extern.slf4j.Slf4j;
 public class AssayService {
 	
 	public List<Map<String, Object>> wrapCcdAssayList(List<CcdAssayList> assayList) {
-	    List<Map<String, Object>> formattedAssays = new ArrayList<>();
+	    Map<Long, Map<String, Object>> assayMap = new LinkedHashMap<>();
 
 	    for (CcdAssayList assay : assayList) {
-	        Map<String, Object> formattedAssay = new LinkedHashMap<>();
+	        Long aeid = assay.getAeid();
+	        Map<String, Object> formattedAssay = assayMap.computeIfAbsent(aeid, id -> {
+	            Map<String, Object> map = new LinkedHashMap<>();
+	            map.put("vendorKey", assay.getVendorKey());
+	            map.put("vendorName", assay.getVendorName());
+	            map.put("assayName", assay.getAssayName());
+	            map.put("aeid", aeid);
+	            map.put("assayComponentName", assay.getAssayComponentName());
+	            map.put("assayComponentEndpointName", assay.getAssayComponentEndpointName());
+	            map.put("assayComponentEndpointDesc", assay.getAssayComponentEndpointDesc());
+	            map.put("ccdAssayDetail", assay.getCcdAssayDetail());
 
-	        formattedAssay.put("vendorKey", assay.getVendorKey());
-	        formattedAssay.put("vendorName", assay.getVendorName());
-	        formattedAssay.put("assayName", assay.getAssayName());
-	        formattedAssay.put("aeid", assay.getAeid());
-	        formattedAssay.put("assayComponentName", assay.getAssayComponentName());
-	        formattedAssay.put("assayComponentEndpointName", assay.getAssayComponentEndpointName());
-	        formattedAssay.put("assayComponentEndpointDesc", assay.getAssayComponentEndpointDesc());
-	        formattedAssay.put("ccdAssayDetail", assay.getCcdAssayDetail());
+	            map.put("geneArray", new ArrayList<Map<String, Object>>());
 
-	        // geneArray
+	            Map<String, Object> singleConc = new LinkedHashMap<>();
+	            singleConc.put("singleConcChemicalCountActive", assay.getSingleConcChemicalCountActive());
+	            singleConc.put("singleConcChemicalCountTotal", assay.getSingleConcChemicalCountTotal());
+	            map.put("singleConc", List.of(singleConc));
+
+	            Map<String, Object> multiConc = new LinkedHashMap<>();
+	            multiConc.put("multiConcChemicalCountActive", assay.getMultiConcChemicalCountActive());
+	            multiConc.put("multiConcChemicalCountTotal", assay.getMultiConcChemicalCountTotal());
+	            map.put("multiConc", List.of(multiConc));
+
+	            return map;
+	        });
+
+	        List<Map<String, Object>> geneArray = (List<Map<String, Object>>) formattedAssay.get("geneArray");
 	        Map<String, Object> geneMap = new LinkedHashMap<>();
 	        geneMap.put("entrezGeneId", assay.getEntrezGeneId());
 	        geneMap.put("geneName", assay.getGeneName());
 	        geneMap.put("geneSymbol", assay.getGeneSymbol());
-	        geneMap.put("commonName", assay.getCommonName());
-	        geneMap.put("taxonName", assay.getTaxonName());
-	        formattedAssay.put("geneArray", List.of(geneMap));
-
-	        // singleConc
-	        Map<String, Object> singleConcMap = new LinkedHashMap<>();
-	        singleConcMap.put("singleConcChemicalCountActive", assay.getSingleConcChemicalCountActive());
-	        singleConcMap.put("singleConcChemicalCountTotal", assay.getSingleConcChemicalCountTotal());
-	        formattedAssay.put("singleConc", List.of(singleConcMap));
-
-	        // multiConc
-	        Map<String, Object> multiConcMap = new LinkedHashMap<>();
-	        multiConcMap.put("multiConcChemicalCountActive", assay.getMultiConcChemicalCountActive());
-	        multiConcMap.put("multiConcChemicalCountTotal", assay.getMultiConcChemicalCountTotal());
-	        formattedAssay.put("multiConc", List.of(multiConcMap));
-
-	        formattedAssays.add(formattedAssay);
+	        geneArray.add(geneMap);
 	    }
 
-	    return formattedAssays;
+	    return new ArrayList<>(assayMap.values());
 	}
 
 }
