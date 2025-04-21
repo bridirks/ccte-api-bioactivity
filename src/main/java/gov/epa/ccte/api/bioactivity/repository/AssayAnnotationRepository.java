a030130b3b291c4cfaf57ec7fe8ee047b2d1746c
Brianna Dirks <Dirks.Brianna@epa.gov>
2025-04-21 10:57:54 -0400

    Create AssayAnnotation by Gene Symbol #91

--- src/main/java/gov/epa/ccte/api/bioactivity/repository/AssayAnnotationRepository.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/repository/AssayAnnotationRepository.java
@@ -2,9 +2,11 @@ package gov.epa.ccte.api.bioactivity.repository;
 
 import gov.epa.ccte.api.bioactivity.domain.AssayAnnotation;
 import gov.epa.ccte.api.bioactivity.projection.assay.AssayAll;
+import gov.epa.ccte.api.bioactivity.projection.assay.AssayEndpointsList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -49,4 +51,25 @@ public interface AssayAnnotationRepository extends JpaRepository<AssayAnnotation
             """, nativeQuery = true)
         <T> List<T> findAssayAnnotations(Class<T> type);
 
+    @Query(value = """
+            SELECT 
+                maa.assay_component_endpoint_name AS assayComponentEndpointName,
+            CASE 
+            	WHEN agg.active_mc IS NOT NULL THEN CONCAT(agg.active_mc, '/', agg.total_mc, '(', CAST(CAST(ROUND((agg.active_mc/agg.total_mc*100),2) AS DECIMAL(5,2)) AS varchar(5)), '%)')
+            END AS multiConcActives,
+            CASE 
+            	WHEN agg.active_sc IS NOT NULL THEN CONCAT(agg.active_sc, '/', agg.total_sc, '(', CAST(CAST(ROUND((agg.active_sc/agg.total_sc*100),2) AS DECIMAL(5,2)) AS varchar(5)), '%)')
+            END AS singleConcActive,
+            	maa.assay_component_endpoint_desc AS assayComponentEndpointDesc,
+                gene_elem->>'gene_symbol' AS geneSymbol
+            FROM 
+                invitro.mv_assay_annotation maa
+            JOIN 
+                invitro.mv_assay_agg agg 
+            ON 
+                maa.aeid = agg.aeid, 
+                json_array_elements(maa.gene) AS gene_elem
+            WHERE gene_elem->>'gene_symbol' = :geneSymbol
+            """, nativeQuery = true)
+        List<AssayEndpointsList> findAssayEndpointsListByGene(@Param("geneSymbol")String geneSymbol);
 }
