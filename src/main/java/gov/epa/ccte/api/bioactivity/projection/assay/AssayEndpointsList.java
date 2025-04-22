ba112371b719ffbe578c6514dd0da5d3f89c161d
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-04-22 11:41:08 -0400

    Create AssayEndpointsList.java
    added projection to fix build

---
+++ src/main/java/gov/epa/ccte/api/bioactivity/projection/assay/AssayEndpointsList.java
@@ -0,0 +1,10 @@
+package gov.epa.ccte.api.bioactivity.projection.assay;
+
+public interface AssayEndpointsList {
+
+    String getAssayComponentEndpointName();
+    String getMultiConcActives();
+    String getsingleConcActive();
+	  String getAssayComponentEndpointDesc();
+    String getGeneSymbol();
+}
