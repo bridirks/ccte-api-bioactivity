13494a028783a131a4fc1574a0141decc7b9b99b
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-04-22 16:48:32 -0400

    Create ToxcastSummaryPlot.java (#101)

---
+++ src/main/java/gov/epa/ccte/api/bioactivity/projection/assay/ToxcastSummaryPlot.java
@@ -0,0 +1,10 @@
+package gov.epa.ccte.api.bioactivity.projection.data;
+
+public interface ToxcastSummaryPlot {
+
+    String getAeid();
+    Float getHitc();
+    Float getAC50();
+    Float getTopOverCutoff();
+
+}
